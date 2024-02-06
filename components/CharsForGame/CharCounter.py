71f330b99439873a041fcb59d690552ae7518931
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2024-02-06 15:51:59 +0100

    Updated the CharsForGame folder

--- components/CharsForGame/CharCounter.py
+++ components/CharsForGame/CharCounter.py
@@ -2,9 +2,31 @@ from typing import List
 
 
 class CharCounter:
-    def __init__(self, chars: List[str]):
+    """
+    #### Description:
+     
+    This class its role runs as a DTO, to retrieve the data, 
+    holds it, until it's needed again.
+    
+    #### Returns: 
+    None in default..
+    """
+
+    def __init__(self, chars: List[str]) -> None:
         self.chars = chars
 
 
-    def count_chars(self) -> int:
+    def get_length_of_chars(self) -> int:
+        """
+        #### Description:
+            retrieve the chars as the total length.
+            
+         NOTE: 
+            the length might diverge as expected, 
+            and should be increased by one outside this
+            scope.
+            
+        #### Returns:
+            A length of chars in a int value
+        """
         return len(self.chars)
--- components/CharsForGame/CharField.py
+++ components/CharsForGame/CharField.py
@@ -2,20 +2,23 @@ from typing import List
 
 
 class CharField():
-    def __init__(self, list_of_chars: List[str]):
-        self.list_of_chars = list_of_chars
-
-
+    _list_of_chars: List[str] = []
+        
     def set_char_input(self, input_value: str) -> None:
         self.letter_to_guess = input_value
 
 
+    def list_of_chars(self) -> List[str]:
+        return self._list_of_chars
+
+
     def field_with_invisible_chars(self, amount_of_chars: int) -> None:
         while amount_of_chars > 1:
-            self.list_of_chars.append("_")
+            self._list_of_chars.append("_")
             amount_of_chars -= 1
 
 
     def get_hidden_word(self, position: int, letter: str) -> List[str]:
-        self.list_of_chars[position] = letter
-        return self.list_of_chars
+        self._list_of_chars[position] = letter
+        return self._list_of_chars
+    
