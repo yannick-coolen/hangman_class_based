f6449e546c636e6fe0232d4d251ef219b77baaf4
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2024-02-06 15:52:57 +0100

    Minor update in class Score

--- components/Score/Score.py
+++ components/Score/Score.py
@@ -1,3 +1,6 @@
+from ..Display.DisplayHangman import DisplayHangman
+
+
 class Score:
     _amount_of_turns = 9
 
@@ -5,9 +8,16 @@ class Score:
         if not answer:
             self._amount_of_turns -= 1
             if self.get_turns() <= 0:
-                return "Game Over"
+                print(
+                    f"""\nGame Over
+                      {DisplayHangman().get_display(self._amount_of_turns)}"""
+                )
             else:
-                return f"Amount of turns left {self.get_turns()}"
+                print(
+                    f"""Amount of turns left {self.get_turns()}
+                      {DisplayHangman().get_display(self._amount_of_turns)}
+                      """
+                )
         else:
             return None
 
