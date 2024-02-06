5eae86a736cab39cfdbcf59924a93f5e2528d831
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2024-02-06 15:52:11 +0100

    Initiate display class

---
+++ components/Display/DisplayHangman.py
@@ -0,0 +1,79 @@
+class DisplayHangman:
+    _index_hangman = [
+        """
+    +
+    |
+    |
+    |
+    |
+    |
+    """,
+        """
+    +-----+
+    |
+    |
+    |
+    |
+    |
+    """,
+        """
+    +-----+
+    |/
+    |
+    |
+    |
+    |
+    """,
+        """
+    +-----+
+    |/    |
+    |
+    |
+    |
+    |
+    """,
+        """
+    +-----+
+    |/    |
+    |     O
+    |
+    |
+    |
+    """,
+        """
+    +-----+
+    |/    |
+    |     O
+    |    -|
+    |
+    |
+    """,
+        """
+    +-----+
+    |/    |
+    |     O
+    |    -|-
+    |
+    |
+    """,
+        """
+    +-----+
+    |/    |
+    |     O
+    |    -|-
+    |    /
+    |
+    """,
+        """
+    +-----+
+    |/    |
+    |     O
+    |    -|-
+    |    / \\
+    |
+    """,
+    ]
+
+    def get_display(self, amount_turns: int) -> str:
+        length_index = len(self._index_hangman) - amount_turns
+        return self._index_hangman[length_index - 1] if length_index > 0 else ""
